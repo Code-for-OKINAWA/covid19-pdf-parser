dfbe5fed5c0e0c5130467e52973c93860af9fcf4
Chris <chris@fliegen.com.tw>
2020-08-13 00:43:16 +0900

    Test action

--- download_report.py
+++ download_report.py
@@ -23,31 +23,31 @@ urllib.request.urlretrieve(download_url, './pdf/' + filename)
 # print(filename)
 
 # Start to parse the PDF
-output_txt = open('data/output.csv', 'w')
-pdf = pdfplumber.open('./pdf/' + filename)
-df=pd.DataFrame(columns=["確定陽性者", "性別", "年齢", "発病日", "確定日", "居住地", "職業", "推定感染経路", ""])
-for page in pdf.pages:
+# output_txt = open('data/output.csv', 'w')
+# pdf = pdfplumber.open('./pdf/' + filename)
+# df=pd.DataFrame(columns=["確定陽性者", "性別", "年齢", "発病日", "確定日", "居住地", "職業", "推定感染経路", ""])
+# for page in pdf.pages:
 
-    # Start convert Table from page 3
-    if page.page_number >= 3:
-        tables = page.extract_tables({
-            "vertical_strategy": "text",
-            "horizontal_strategy": "lines",
-            "intersection_y_tolerance": 15,
-        })
+#     # Start convert Table from page 3
+#     if page.page_number >= 3:
+#         tables = page.extract_tables({
+#             "vertical_strategy": "text",
+#             "horizontal_strategy": "lines",
+#             "intersection_y_tolerance": 15,
+#         })
 
-        # print(tables)
-        for table in tables:
-            localDf = pd.DataFrame(table, columns=["確定陽性者", "性別", "年齢", "発病日", "確定日", "居住地", "職業", "推定感染経路",""])
-            localDf = localDf.replace('\n','', regex=True)
+#         # print(tables)
+#         for table in tables:
+#             localDf = pd.DataFrame(table, columns=["確定陽性者", "性別", "年齢", "発病日", "確定日", "居住地", "職業", "推定感染経路",""])
+#             localDf = localDf.replace('\n','', regex=True)
 
-            # Remove each page's header row
-            indexNames = localDf[ localDf['確定陽性者'] == "確定陽性者" ].index
-            localDf.drop(indexNames , inplace=True)
+#             # Remove each page's header row
+#             indexNames = localDf[ localDf['確定陽性者'] == "確定陽性者" ].index
+#             localDf.drop(indexNames , inplace=True)
 
-            # TODO: Replace date format
+#             # TODO: Replace date format
 
-            df = df.append(localDf)
+#             df = df.append(localDf)
 
-df.to_csv(output_txt, index=False, header=True)
+# df.to_csv(output_txt, index=False, header=True)
 
