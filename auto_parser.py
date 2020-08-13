e7b48d7cd4fe11b7b94adbc5c7e4dec5be029d63
amouro <amouro@gmail.com>
2020-08-13 13:45:58 +0900

    Replace date format

--- auto_parser.py
+++ auto_parser.py
@@ -33,11 +33,11 @@ pdf = pdfplumber.open('./pdf/' + filename)
 df = pd.DataFrame(columns=["確定陽性者", "性別", "年齢", "発病日", "確定日", "居住地", "職業", "推定感染経路"])
 df = pd.DataFrame()
 for page in pdf.pages:
-    # clean up the invisible text hidden by the clips
-    cleanPage = page.filter(lambda obj: obj["non_stroking_color"] != (1,1,1))
-
     # Start convert Table from page 3
-    if cleanPage.page_number >= 3:
+    if page.page_number >= 3:
+        # clean up the invisible text hidden by the clips
+        cleanPage = page.filter(lambda obj: obj["non_stroking_color"] != (1,1,1))
+
         tables = cleanPage.extract_tables({
             "vertical_strategy": "text",
             "horizontal_strategy": "lines",
@@ -54,7 +54,11 @@ for page in pdf.pages:
             localDf.drop(indexNames , inplace=True)
 
             # TODO: Replace date format
-
+            prepend_year = '2020'
+            find_pattern = r"^(?P<m>\d*)月(?P<d>\d*)\D*"
+            replace_pattern = lambda date: prepend_year + '/' + date.group('m') + '/' + date.group('d')
+            localDf['発病日'] = localDf['発病日'].str.replace(find_pattern, replace_pattern, regex=True)
+            localDf['確定日'] = localDf['確定日'].str.replace(find_pattern, replace_pattern, regex=True)
             df = df.append(localDf)
 
 df.to_csv(output_txt, index=False, header=True)
