50277c4963eaa48e9e68185e2327a05482405418
amouro <amouro@gmail.com>
2020-08-20 22:20:47 +0900

    Fix auto parser

--- auto_parser.py
+++ auto_parser.py
@@ -41,7 +41,8 @@ for page in pdf.pages:
     # Start convert Table from page 2
     if page.page_number >= 2:
         # clean up the invisible text hidden by the clips
-        cleanPage = page.filter(lambda obj: obj["non_stroking_color"] != (1,1,1))
+        # print(page.chars)
+        cleanPage = page.filter(lambda obj: obj["non_stroking_color"] not in [1, (1,1,1)])
 
         tables = cleanPage.extract_tables({
             "vertical_strategy": "text",
@@ -62,7 +63,6 @@ for page in pdf.pages:
             indexNames4 = localDf[ localDf['確定陽性者'] == "" ].index
             indexNames5 = localDf[ localDf['性別'] == "欠番" ].index
             indexNames6 = localDf[ localDf['性別'] == "" ].index
-            print(indexNames6)
             
             if not indexNames1.empty:
                 indexNames.append(indexNames1.item())
@@ -77,8 +77,6 @@ for page in pdf.pages:
             if not indexNames6.empty:
                 indexNames.append(indexNames6.item())
 
-
-            print (indexNames)
             localDf.drop(indexNames , inplace=True)
 
             # TODO: Replace date format
@@ -89,9 +87,9 @@ for page in pdf.pages:
             localDf['確定日'] = localDf['確定日'].str.replace(find_pattern, replace_pattern, regex=True)
             df = df.append(localDf)
 
-            if page.page_number == 88:
-                # print(localDf.loc[21,:])
-                print(localDf)
+            # if page.page_number == 61:
+            #     # print(localDf.loc[21,:])
+            #     print(localDf)
 
 # Create a report
 now = datetime.now()
